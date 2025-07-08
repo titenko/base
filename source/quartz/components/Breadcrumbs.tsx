b5947d7edd27f19351bab5d03d4c208ff995bbac
titenko <titenko.m@gmail.com>
2025-07-08 02:16:03 +0200

    vault backup: 2025-07-08 02:16:03

--- source/quartz/components/Breadcrumbs.tsx
+++ source/quartz/components/Breadcrumbs.tsx
@@ -65,7 +65,10 @@ export default ((opts?: Partial<BreadcrumbOptions>) => {
     }
 
     // Format entry for root element
-    const firstEntry = formatCrumb(options.rootName, fileData.slug!, "/" as SimpleSlug)
+    const firstEntry = {
+    displayName: options.rootName,
+    path: "/base/",
+    }
     const crumbs: CrumbData[] = [firstEntry]
 
     if (!folderIndex && options.resolveFrontmatterTitle) {
