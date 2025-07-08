db276cf14b7463e04b58cb6d63d82a91abc27760
titenko <titenko.m@gmail.com>
2025-07-08 02:47:03 +0200

    vault backup: 2025-07-08 02:47:03

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🤓 Quartz 4.0",
+    pageTitle: "📝 Quartz 4.0",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
